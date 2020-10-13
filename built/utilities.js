8807ca7c48ab06568834979f68856038421e1cab
Rafal <rafal.czerniawski@hexcustody.com>
2020-10-13 12:29:33 +0800

    Update built

--- built/utilities.js
+++ built/utilities.js
@@ -6,15 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const crypto_1 = __importDefault(require("crypto"));
 class Utilities {
     static toUri(parsedUrl, path) {
-        let separator = parsedUrl.slashes ? '/' : '\\';
+        const separator = parsedUrl.slashes ? '/' : '\\';
         return [
             `${parsedUrl.protocol}${separator}`,
             parsedUrl.host,
-            parsedUrl.slashes ? path.replace(/^\//, '') : path.replace(/^\\/, '')
+            parsedUrl.slashes ? path.replace(/^\//, '') : path.replace(/^\\/, ''),
         ].join(separator);
     }
     static getTime() {
-        return (new Date()).getTime();
+        return new Date().getTime();
     }
     static padding(value, paddingChar = '0', pad = 4) {
         value = value.toString();
@@ -31,13 +31,19 @@ class Utilities {
     }
     static genDigest(body) {
         let msg = '';
-        if (typeof (body) === 'object') {
+        if (typeof body === 'object') {
             msg = JSON.stringify(body);
         }
-        return `SHA-256=${crypto_1.default.createHash("sha256").update(msg, 'utf8').digest('base64')}`;
+        return `SHA-256=${crypto_1.default
+            .createHash('sha256')
+            .update(msg, 'utf8')
+            .digest('base64')}`;
     }
     static genSig(toSign, secret) {
-        return crypto_1.default.createHmac("sha512", secret).update(toSign).digest("base64");
+        return crypto_1.default
+            .createHmac('sha512', secret)
+            .update(toSign)
+            .digest('base64');
     }
     static genAuthorizationString(method, host, path, body, nonce, secret, apiKey) {
         const requestLine = `${method} ${path} HTTP/1.1`;
@@ -51,7 +57,7 @@ class Utilities {
         return `hmac username="${apiKey}", algorithm="hmac-sha512", headers="nonce host${method === 'POST' ? ' digest ' : ' '}request-line", signature="${Utilities.genSig(stringToSign, secret)}"`;
     }
     static timeStampHeader(options) {
-        options['headers']['date'] = (new Date()).toUTCString();
+        options.headers.date = new Date().toUTCString();
         return options;
     }
 }
